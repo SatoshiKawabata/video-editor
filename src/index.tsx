8af8f173258268fb1b40244eec414876a6c284b9
Satoshi Kawabata <satoshi.kawabata@nulab-inc.com>
2020-02-07 13:07:43 +0900

    Make parcel typescript react project

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const Application = () => {
+  return <h1>Hello world!</h1>;
+};
+ReactDOM.render(<Application />, document.getElementById("app"));
