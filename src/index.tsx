7cf7fcb5932e8540bce41d9b2a743453e806afc3
Satoshi Kawabata <satoshi.kawabata@nulab-inc.com>
2020-02-07 13:20:07 +0900

    add component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,58 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+type State = {
+  videoSrc: string;
+  audioSrc: string;
+  downloadLink: string;
+};
+
 const Application = () => {
-  return <h1>Hello world!</h1>;
+  const [state, setState] = React.useState<State>({
+    videoSrc: "",
+    audioSrc: "",
+    downloadLink: ""
+  });
+  const videoRef = React.createRef<HTMLVideoElement>();
+  const audioRef = React.createRef<HTMLAudioElement>();
+  return (
+    <>
+      <input
+        type="file"
+        multiple
+        onChange={e => {
+          if (e.target && e.target.files) {
+            const file = e.target.files[0];
+            console.log(file);
+            var fileURL = URL.createObjectURL(file);
+            setState({
+              ...state,
+              videoSrc: fileURL,
+              audioSrc: fileURL
+            });
+          }
+        }}
+      />
+      <video
+        ref={videoRef}
+        src={state.videoSrc}
+        onEnded={() => {
+          console.log("onended");
+        }}
+      ></video>
+      <audio ref={audioRef} src={state.audioSrc}></audio>
+      <button
+        type="button"
+        onClick={() => {
+          videoRef.current?.play();
+          audioRef.current?.play();
+        }}
+      >
+        Start recording
+      </button>
+      <canvas ref="canvas"></canvas>
+      <a href={state.downloadLink}>download</a>
+    </>
+  );
 };
 ReactDOM.render(<Application />, document.getElementById("app"));
